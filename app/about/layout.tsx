aeba0095abae522ec82f10326351af8312a75c66
badrGADI <badr.gadi.123@gmail.com>
2025-08-13 01:43:20 +0200

    add |

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Our Family Farm in Morocco | Sustainable & Organic Agriculture",
+  title: "Our Family Farm in Morocco || Sustainable & Organic Agriculture",
   description:
     "Learn about our story, values, and commitment to sustainable farming. Our farm in Morocco grows top-quality fruits and vegetables for local and international markets.",
   keywords:
