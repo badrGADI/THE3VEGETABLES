23492fd1efc8bd857d4d74ae01760b2ea23cf395
badrGADI <badr.gadi.123@gmail.com>
2025-08-12 17:34:50 +0200

    wazani tla9 djaja lmaliha nod tkhdam wala nji n7wik liha

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Our Family Farm in Morocco | Sustainable & Organic Agriculture",
   description:
-    "Learn about our story, values, and commitment to sustainable farming. Our farm in Morocco grows top-quality fruits and vegetables for local and international market.",
+    "Learn about our story, values, and commitment to sustainable farming. Our farm in Morocco grows top-quality fruits and vegetables for local and international markets.",
   keywords:
     "moroccan family farm, sustainable agriculture, organic farming morocco, farm story, agricultural practices",
 };
